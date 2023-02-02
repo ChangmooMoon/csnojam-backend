2291b5ef0e02d3e6eb9a676a9d0f0b08ff4b6641
changmoo <public.ellerymoon@gmail.com>
2023-02-02 19:35:46 +0900

    add: dependency 추가

---
+++ src/test/java/csnojam/example/csnojam/CsnojamApplicationTests.java
@@ -0,0 +1,13 @@
+package csnojam.example.csnojam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsnojamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
