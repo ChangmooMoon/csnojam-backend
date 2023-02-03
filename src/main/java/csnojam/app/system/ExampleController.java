1ade56bbb09bd7d895bfba96bd193c45dccd6af2
changmoo <public.ellerymoon@gmail.com>
2023-02-03 18:39:08 +0900

    fix: 프로젝트 구조 DDD 기반으로 변경

--- src/main/java/csnojam/app/system/ExampleController.java
+++ src/main/java/csnojam/app/system/ExampleController.java
@@ -1,4 +1,4 @@
-package csnojam.app.web;
+package csnojam.app.system;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- src/test/java/csnojam/app/system/ExampleControllerTest.java
+++ src/test/java/csnojam/app/system/ExampleControllerTest.java
@@ -1,8 +1,10 @@
-package csnojam.app.web;
+package csnojam.app.system;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +15,14 @@ class ExampleControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
     @Test
     void test1() throws Exception {
