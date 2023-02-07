1191be7590db82415a13c1ecd05eb4e0d0f63d56
HoYoon-Lee <happylhy0630@naver.com>
2023-02-07 18:14:00 +0900

    [REFACTOR] DTO에 @Builder 추가 (for Test)

--- src/main/java/csnojam/app/problem/dto/UpdateProblemDto.java
+++ src/main/java/csnojam/app/problem/dto/UpdateProblemDto.java
@@ -1,8 +1,10 @@
 package csnojam.app.problem.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UpdateProblemDto {
     private final Long categoryId;
     private final String text;
