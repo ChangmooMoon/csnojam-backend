b21b8b461007f0986d92d74a54aa11d480d7dd18
HoYoon-Lee <happylhy0630@naver.com>
2023-02-17 02:18:52 +0900

    test: 문서화를 위한 설정 추가

--- src/test/java/csnojam/app/common/ControllerTest.java
+++ src/test/java/csnojam/app/common/ControllerTest.java
@@ -3,29 +3,33 @@ package csnojam.app.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+@ExtendWith(RestDocumentationExtension.class)
 @ComponentScan({"csnojam.app.config", "csnojam.app.jwt"})
 public abstract class ControllerTest {
     protected MockMvc mockMvc;
 
     protected ObjectMapper objectMapper;
 
-    @Autowired
-    protected WebApplicationContext webApplicationContext;
-
     @BeforeEach
-    void setUp(){
-        mockMvc = webAppContextSetup(webApplicationContext).addFilter(((request, response, chain) -> {
-            request.setCharacterEncoding("utf-8");
-            response.setCharacterEncoding("utf-8");
-            chain.doFilter(request, response);
-        })).build();
+    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentationContextProvider) {
+        mockMvc = webAppContextSetup(webApplicationContext)
+                .addFilter(((request, response, chain) -> {
+                    request.setCharacterEncoding("utf-8");
+                    response.setCharacterEncoding("utf-8");
+                    chain.doFilter(request, response);
+                }))
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .build();
 
     }
 
