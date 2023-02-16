cd7804733c8d36948016bd9bd0774887565068ac
HoYoon-Lee <happylhy0630@naver.com>
2023-02-17 01:14:09 +0900

    feat: StatusMessage에 VALID_FIELD, INVALID_FIELD 추가

--- src/main/java/csnojam/app/common/response/StatusMessage.java
+++ src/main/java/csnojam/app/common/response/StatusMessage.java
@@ -4,11 +4,18 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.OK;
+
 @Getter
 @RequiredArgsConstructor
 public enum StatusMessage {
     // 200 OK
-    SUCCESS(HttpStatus.OK, "요청을 성공적으로 처리했습니다"),
+    SUCCESS(OK, "요청을 성공적으로 처리했습니다"),
+    VALID_FIELD(OK, "사용가능합니다"),
+
+    // 409 CONFLICT
+    INVALID_FIELD(CONFLICT, "중복된 데이터가 존재합니다")
 
     ;
 
