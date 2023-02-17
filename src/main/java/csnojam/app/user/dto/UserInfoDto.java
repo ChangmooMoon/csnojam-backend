8eb992097090e05f996b7f8b72bb0c27ab126cd9
HoYoon-Lee <happylhy0630@naver.com>
2023-02-18 01:01:39 +0900

    feat: 유저 정보를 반환하기 위한 UserInfoDto 생성

---
+++ src/main/java/csnojam/app/user/dto/UserInfoDto.java
@@ -0,0 +1,21 @@
+package csnojam.app.user.dto;
+
+import csnojam.app.user.User;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserInfoDto {
+    private final String email;
+    private final String nickname;
+    private final String profileUrl;
+
+    public UserInfoDto of(User user) {
+        return UserInfoDto.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .profileUrl(user.getProfileUrl())
+                .build();
+    }
+}
