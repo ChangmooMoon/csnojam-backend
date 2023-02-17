93ded108a2ed2f43a91612d7f625af34a693ac89
HoYoon-Lee <happylhy0630@naver.com>
2023-02-18 01:18:40 +0900

    feat: StatusMessage에 USER_NOT_FOUND 추가

--- src/main/java/csnojam/app/common/response/StatusMessage.java
+++ src/main/java/csnojam/app/common/response/StatusMessage.java
@@ -13,11 +13,14 @@ public enum StatusMessage {
     SUCCESS(OK, "요청을 성공적으로 처리했습니다"),
     VALID_FIELD(OK, "사용가능합니다"),
 
+    // 404 NOT_FOUND
+    USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다"),
+
     // 409 CONFLICT
     INVALID_FIELD(CONFLICT, "중복된 데이터가 존재합니다"),
 
     // 500 INTERNAL_SERVER_ERROR
-    ERROR(INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 에러가 발생했습니다")
+    ERROR(INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 에러가 발생했습니다"),
     ;
 
     private final HttpStatus httpStatus;
