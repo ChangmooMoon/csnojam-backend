98bc5668a4bffa5ba2b63eac0617a52add95edd5
RoxyYujinKim <yujinkim0707@gmail.com>
2023-02-20 10:53:25 +0900

    feat: 로그인/회원가입 실패 시 필요한 상태 메시지 구현

--- src/main/java/csnojam/app/common/response/StatusMessage.java
+++ src/main/java/csnojam/app/common/response/StatusMessage.java
@@ -13,6 +13,11 @@ public enum StatusMessage {
     SUCCESS(OK, "요청을 성공적으로 처리했습니다"),
     VALID_FIELD(OK, "사용가능합니다"),
 
+    // 400 Error
+    LOGIN_FAIL(BAD_REQUEST, "로그인에 실패했습니다."),
+    EMPTY_EMAIL(BAD_REQUEST, "이메일을 입력해주세요."),
+    EMPTY_PASSWORD(BAD_REQUEST, "비밀번호를 입력해주세요"),
+
     // 404 NOT_FOUND
     USER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다"),
 
