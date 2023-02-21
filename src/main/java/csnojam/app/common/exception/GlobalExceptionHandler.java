e79b3eb95af54c8939dc24f52464e1a0197df584
RoxyYujinKim <yujinkim0707@gmail.com>
2023-02-21 20:47:16 +0900

    feat: MethodArgumentException 관련 global 예외처리 메소드 작성

--- src/main/java/csnojam/app/common/exception/GlobalExceptionHandler.java
+++ src/main/java/csnojam/app/common/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package csnojam.app.common.exception;
 import csnojam.app.common.response.ApiResponse;
 import csnojam.app.common.response.StatusMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,5 +20,14 @@ public class GlobalExceptionHandler {
         StatusMessage statusMessage = e.getStatusMessage();
         log.error(String.format("ApiException: %s - %d", statusMessage.getMessage(), statusMessage.getHttpStatus().value()));
         return ApiResponse.withNothing(e.getStatusMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException e){
+        Map<String, String> errorMessage = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessage.put("message", error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errorMessage, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
