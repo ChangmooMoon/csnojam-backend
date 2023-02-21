a83706f9c63c5cd9a1d78b333a218ca8b407a16c
RoxyYujinKim <yujinkim0707@gmail.com>
2023-02-21 20:50:58 +0900

    refactor: @Valid를 추가하여 입력 Parameter 로그 메시지 삭제

--- src/main/java/csnojam/app/user/UserController.java
+++ src/main/java/csnojam/app/user/UserController.java
@@ -29,42 +29,20 @@ public class UserController {
     private final JwtProvider jwtProvider;
 
     @PostMapping("sign-up")
-    public ResponseEntity<?> join(@RequestBody UserJoinDto userJoinDto){
+    public ResponseEntity<?> join(@RequestBody @Valid UserJoinDto userJoinDto){
         ResponseEntity<?> responseEntity;
-        System.out.println("UserController: join 진입");
-        if (userJoinDto.getEmail() == null) {
-            responseEntity = ApiResponse.withNothing(EMPTY_EMAIL);
-            return responseEntity;
-        }
-        if (userJoinDto.getPassword() == null){
-            responseEntity = ApiResponse.withNothing(EMPTY_PASSWORD);
-            return responseEntity;
-        }
-        try {
-            userService.join(userJoinDto);
-            responseEntity = ApiResponse.withNothing(SUCCESS);
-            return responseEntity;
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            responseEntity = ApiResponse.withNothing(ERROR);
-            return responseEntity;
-        }
+        userService.join(userJoinDto);
+        responseEntity = ApiResponse.withNothing(SUCCESS);
+        return responseEntity;
     }
 
     @PostMapping("sign-in")
-    public ResponseEntity<?> login(@RequestBody UserLoginDto userLoginDto, HttpServletResponse httpServletResponse){
+    public ResponseEntity<?> login(@RequestBody @Valid UserLoginDto userLoginDto, HttpServletResponse httpServletResponse){
         ResponseEntity<?> responseEntity;
-        try {
-            System.out.println("UserController: login 진입");
-            String token  = userService.login(userLoginDto);
-            jwtProvider.writeTokenResponse(httpServletResponse, token);
-            responseEntity = ApiResponse.withNothing(SUCCESS);
-            return responseEntity;
-        } catch (Exception e){
-            log.error(e.getMessage());
-            responseEntity = ApiResponse.withNothing(LOGIN_FAIL);
-            return responseEntity;
-        }
+        String token  = userService.login(userLoginDto);
+        jwtProvider.writeTokenResponse(httpServletResponse, token);
+        responseEntity = ApiResponse.withNothing(SUCCESS);
+        return responseEntity;
     }
 
     @GetMapping("validate/{field}")
